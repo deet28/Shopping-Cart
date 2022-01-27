ce67e914a772454c661116fdb134605ad13bdc15
David Turchetta <davidturchetta@outlook.com>
2022-01-27 14:05:07 -0500

    Added shopping cart. Created media folder. Stylized nav bar.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Home from "./Home";
 import Shop from "./Shop";
-
+import ShoppingCart from '../media/ShoppingCart.png';
 
 function Nav() {
  return (
-  <ul className = "nav-bar">
-    <Link className = "nav-item" to = "/">
-      <li>Home</li>
-    </Link>
-    <Link className = "nav-item" to = "/Shop">
-      <li>Shop</li> 
-    </Link>
-     <li>About</li>
-  </ul>
+  <>
+  <div className = "nav-main">
+    <h2 className = "store-name">Store</h2>
+    <ul className = "nav-bar">
+      <Link className = "nav-item" to = "/">
+        <li>Home</li>
+      </Link>
+      <Link className = "nav-item" to = "/Shop">
+        <li>Shop</li> 
+      </Link>
+      <Link className = "nav-item" to = "/Shop">
+        <li>About</li>
+      </Link>
+      <Link className = "nav-item" to = "/Shop">
+        <li 
+        className = "nav-cart"> 
+          <img 
+            src = {ShoppingCart} 
+            className = "cart" >
+          </img>
+        </li>
+      </Link>
+    </ul>
+  </div>
+  </>
   )
 }
 
