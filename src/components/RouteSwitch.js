a8f6edb27b66989dee08642a46ac705f9eaddd5d
David Turchetta <davidturchetta@outlook.com>
2022-03-20 12:39:04 -0400

    Working on routes.

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../App.css';
 import Nav from './Nav';
-import Home from "./Home";
-import Shop from "./Shop";
-import About from "./About";
-import Cart from "./Cart";
-import Footer from "./Footer";
+import Home from './Home';
+import Shop from './Shop';
+import About from './About';
+import Cart from './Cart';
+import Footer from './Footer';
 
 
 
@@ -17,8 +17,8 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path = "/cart" element = {<Cart />}/>
         <Route path = "/" element = {<Home  />}/>
+        <Route path = "/cart" element = {<Cart />}/>
         <Route path = "/shop" element = {<Shop />} />
         <Route path = "/about" element = {<About />}/>
       </Routes>
